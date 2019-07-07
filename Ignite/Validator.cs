07c20b7a6132f0a0b773d1a893543b82c6b86284
Carlos Herasme <rcherasmeestrada@gmail.com>
2019-07-06 22:47:03 -0400

    Validator class created with validate file path function integrated.

---
+++ Ignite/Validator.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+[assembly: InternalsVisibleTo("IgniteTest")]
+namespace Ignite
+{
+    internal class Validator
+    {
+        internal (bool Result, string FileErrorInfoMessage) ValidateFileInfo(string fileName, string filePath)
+        {
+            bool wellFileInfo = true;
+            FileErrorInfo fileError = FileErrorInfo.None;
+
+
+            if (!Directory.Exists(filePath))
+            {
+                wellFileInfo = false;
+                fileError =  FileErrorInfo.Path;
+            }
+            else if (!File.Exists(fileName))
+            {
+                wellFileInfo = false;
+                fileError = FileErrorInfo.File;
+            }
+
+            string fileErrorInfoMessage;
+
+            if (fileError == FileErrorInfo.None)
+                fileErrorInfoMessage = "Without errors";
+            else
+            {
+                fileErrorInfoMessage = fileError == FileErrorInfo.File
+                    ? "The information provided about the file name/extension is wrong."
+                    : "The information provided about the file directory is wrong.";
+            }
+
+            return (wellFileInfo, fileErrorInfoMessage);
+        }
+    }
+
+    internal enum FileErrorInfo
+    {
+        File,
+        Path,
+        None
+    }
+}
---
+++ IgniteTest/ValidatorTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ignite;
+using System.IO;
+
+namespace IgniteTest
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        internal Validator Validator { get; } = new Validator();
+        readonly string executionDirectory, currentDirectory;
+
+        public ValidatorTest()
+        {
+            executionDirectory = Environment.CurrentDirectory;
+            string binDirectory = Directory.GetParent(executionDirectory).Parent.FullName;
+            currentDirectory = Directory.GetParent(binDirectory).FullName;
+        }
+
+        [TestMethod]
+        public void ValidateFilePath_NonExistingPath_False()
+        {
+            var fileName = "rojo";
+            var filePath = "azul";
+            var wanted = false;
+
+            var (result, errorMessage) = Validator.ValidateFileInfo(fileName, filePath);
+
+            Assert.AreEqual(wanted, result);
+        }
+
+        [TestMethod]
+        public void ValidateFilePath_BadFile_False()
+        {
+            var fileName = "rojo";
+            var filePath = Path.Combine(currentDirectory, @"TestData");
+            var wanted = false;
+
+            var (result, errorMessage) = Validator.ValidateFileInfo(fileName, filePath);
+
+            Assert.AreEqual(wanted, result);
+        }
+
+        [TestMethod]
+        public void ValidateFilePath_WellData_True()
+        {
+            var fileName = "Test.xlsx";
+            var filePath = Path.Combine(currentDirectory, @"TestData");
+            var wanted = false;
+
+            var (result, errorMessage) = Validator.ValidateFileInfo(fileName, filePath);
+
+            Assert.AreEqual(wanted, result);
+        }
+    }
+}
